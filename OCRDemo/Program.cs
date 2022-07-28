4b1acf9b5cbda4bcfce9857576b38e5f3ba63f44
Jeshal2013 <52350930+Jeshal2013@users.noreply.github.com>
2022-07-28 10:52:49 +0530

    Add project files.

---
+++ OCRDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+namespace OCRDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "F:\\Demo\\OCRDemo\\OCRDemo\\Images\\Hospital3.jpg";
+           //var path= AppContext.BaseDirectory+ "Images\\Hospital3.jpg";
+            var Ocr = new IronTesseract();
+            using (var Input = new OcrInput(path))
+            {
+                // Input.Deskew();  // use if image not straight
+                // Input.DeNoise(); // use if image contains digital noise
+                var Result = Ocr.Read(Input);
+                Console.WriteLine(Result.Text);
+            }
+        }
+    }
+}
